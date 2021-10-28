27ecd9342e4e131f3e9e0ac2081d9f00c5bbbeda
Colin McMahon <colin.mcmahon@canadalife.de>
2021-10-28 17:10:30 +0100

    version2

--- src/test/java/MyTests/ExampleUsageTest.java
+++ src/test/java/MyTests/ExampleUsageTest.java
@@ -1,17 +1,24 @@
 package MyTests;
 
+import com.mailslurp.apis.InboxControllerApi;
 import com.mailslurp.clients.ApiClient;
+import com.mailslurp.clients.ApiException;
+import com.mailslurp.models.Inbox;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.concurrent.TimeUnit;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ExampleUsageTest {
 
     private static WebDriver driver = null;
@@ -46,12 +53,58 @@ public class ExampleUsageTest {
 
     private static final String PLAYGROUND_URL = "https://playground.mailslurp.com";
 
+
+
     @Test
     public void test1_canLoadAuthenticationPlayground() {
 
         driver.get(PLAYGROUND_URL);
         assertEquals(driver.getTitle(), "React App");
+
+
+
     }
+
+    @Test
+    public void hello(){
+        System.out.println("hello");
+        driver.findElement(By.cssSelector("[data-test=sign-in-create-account-link]")).click();
+    }
+
+    private static Inbox inbox;
+
+    @Test
+    public void test3_canCreateEmailAddressAndSignUp() throws ApiException {
+        // create a real, randomized email address with MailSlurp to represent a user
+        InboxControllerApi inboxControllerApi = new InboxControllerApi(mailslurpClient);
+        inbox = inboxControllerApi.createInbox(null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null);
+
+        // check the inbox was created
+        assertNotNull(inbox.getId());
+        assertTrue(inbox.getEmailAddress().contains("@mailslurp.com"));
+
+        // fill the playground app's sign-up form with the MailSlurp
+        // email address and a random password
+        driver.findElement(By.name("email")).sendKeys(inbox.getEmailAddress());
+        driver.findElement(By.name("password")).sendKeys("Qwerty_12");
+
+        // submit the form to trigger the playground's email confirmation process
+        // we will need to receive the confirmation email and extract a code
+        driver.findElement(By.cssSelector("[data-test=sign-up-create-account-button]")).click();
+    }
+
+
+
+
 
 
 }
